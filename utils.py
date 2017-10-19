470f80499ce551114d94786299e8336f3d15e943
Jerry Chou <jjery2243542@gmail.com>
2017-10-19 20:17:26 +0800

    do some optimize

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 import argparse
 import random
+import time
 
 class Hps(object):
     def __init__(self):
@@ -43,6 +44,7 @@ class DataLoader(object):
         self.max_step = max_step
         self.read_sex_file(speaker_sex_path)
         self.speakers = list(self.f_h5['train'].keys())
+        self.speaker2utts = {speaker:list(self.f_h5['train/{}'.format(speaker)].keys()) for speaker in self.speakers}
 
     def read_sex_file(self, speaker_sex_path):
         with open(speaker_sex_path, 'r') as f:
@@ -54,42 +56,51 @@ class DataLoader(object):
             self.male_ids = f.readline().strip().split()
 
     def sample_utt(self, speaker_id):
-        utt_ids = list(self.f_h5['train/{}'.format(speaker_id)].keys())
         # sample an utterence
-        utt_id = random.sample(utt_ids, 1)[0]
+        utt_id = self.rand(self.speaker2utts[speaker_id])
         spec = self.f_h5['train/{}/{}'.format(speaker_id, utt_id)]
         return spec
-        
+
+    def rand(self, l):
+        rand_idx = random.randint(0, len(l) - 1)
+        return l[rand_idx] 
+
     def sample(self):
         # sample two speakers
         #speakerA, speakerB = random.sample(self.speakers, 2)
-        speakerA = random.sample(self.female_ids, 1)[0]
-        speakerB = random.sample(self.male_ids)[0]
+        speakerA = self.rand(self.female_ids)
+        speakerB = self.rand(self.male_ids)
         specA = self.sample_utt(speakerA)
         # sample t and t^k 
         t = random.randint(0, specA.shape[0] - 2)
         t_k = random.randint(t, min(specA.shape[0] - 1, t + self.max_step))
         # sample a segment from speakerB
         specB = self.sample_utt(speakerB)
-        segB = random.sample(specB, 1)[0]
-        return specA[t][0:1], specA[t_k][0:1], segB[0:1] 
+        j = random.randint(0, specB.shape[0] - 1)
+        return specA[t][0:1], specA[t_k][0:1], specB[j][0:1] 
 
-    def next_batch(self):
+    def __iter__(self):
+        return self
+
+    def __next__(self):
         all_X = [[], [], []]
-        for i in range(self.batch_size):
+        for _ in range(self.batch_size):
             for X, x in zip(all_X, self.sample()):
                 X.append(x) 
         return [np.array(X, dtype=np.float32) for X in all_X]
 
 
 if __name__ == '__main__':
-    hps = Hps()
-    hps.dump('./hps/v1.json')
-    #data_loader = DataLoader(
-    #    '/storage/raw_feature/voice_conversion/libre_equal.h5',
-    #    '/storage/raw_feature/voice_conversion/train-clean-100-speaker-sex.txt',
-    #)
-    #for i in range(100):
-    #    print(i)
-    #    _ = data_loader.next_batch()
+    #hps = Hps()
+    #hps.dump('./hps/v1.json')
+    data_loader = DataLoader(
+        '/storage/raw_feature/voice_conversion/libre_equal.h5',
+        '/storage/raw_feature/voice_conversion/train-clean-100-speaker-sex.txt',
+    )
+    st = time.time()
+    for i in range(100):
+        print(i)
+        batch = next(data_loader)
+    et = time.time()
+    print(et - st)
 
