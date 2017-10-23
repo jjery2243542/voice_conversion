7449704a481fe799c1ef20d7caeab773bff335d9
Jerry Chou <b02902054@ntu.edu.tw>
2017-10-23 12:43:24 +0800

    change save model

--- solver.py
+++ solver.py
@@ -11,6 +11,12 @@ from utils import DataLoader
 from utils import Logger
 import os 
 
+def mean_grad(net):
+    grad = Variable(torch.FloatTensor([0])).cuda()
+    for i, p in enumerate(net.parameters()):
+        grad += torch.mean(p.grad)
+    return grad / (i + 1)
+
 class Solver(object):
     def __init__(self, hps, data_loader, log_dir='./log/'):
         self.hps = hps
@@ -46,49 +52,27 @@ class Solver(object):
         x = Variable(torch.from_numpy(x), requires_grad=True)
         return x.cuda() if torch.cuda.is_available() else x
 
-    def pretrain_discri(self, model_path):
-        # use spectrum pretrain
-        for iteration in range(self.hps.pretrain_iterations):
-            X_i_t, X_i_tk, X_j = [self.to_var(x) for x in next(self.data_loader)]
-            D_same = self.Discriminator(X_i_t, X_i_tk)
-            D_diff = self.Discriminator(X_i_t, X_j)
-            loss = -torch.mean(torch.log(D_same) + torch.log(1 - D_diff))
-            self.reset_grad()
-            loss.backward()
-            self.D_opt.step()
-            slot_value = (
-                iteration + 1,
-                self.hps.pretrain_iterations, 
-                loss.data[0]
-            )
-            print('iteration: [%04d/%04d], loss: %.6f' % slot_value)
-            info = {
-                'pretrain_loss': loss.data[0],
+    def save_model(self, model_path, iteration, enc_only=True):
+        if not enc_only:
+            all_model = {
+                'encoder_s': self.Encoder_s.state_dict(),
+                'encoder_c': self.Encoder_c.state_dict(),
+                'decoder': self.Decoder.state_dict(),
+                'discriminator': self.Discriminator.state_dict(),
             }
-            for tag, value in info.items():
-                self.logger.scalar_summary(tag, value, iteration + 1)
-
-            if iteration % 500 == 0 or iteration + 1 >= self.hps.pretrain_iterations:
-                self.save_model(model_path, iteration, dis_only=True)
-
-    def save_model(self, model_path, iteration, dis_only=False):
-        if not dis_only:
-            Es_path = os.path.join(model_path, 'Es.pkl-{}'.format(iteration))
-            Ec_path = os.path.join(model_path, 'Ec.pkl-{}'.format(iteration))
-            Dec_path = os.path.join(model_path, 'Dec.pkl-{}'.format(iteration))
-        Dis_path = os.path.join(model_path, 'Dis.pkl-{}'.format(iteration))
-        if not dis_only:
-            torch.save(self.Encoder_s.state_dict(), Es_path)
-            torch.save(self.Encoder_c.state_dict(), Ec_path)
-            torch.save(self.Decoder.state_dict(), Dec_path)
-        torch.save(self.Discriminator.state_dict(), Dis_path)
-        if not dis_only:
-            self.model_kept.append([Es_path, Ec_path, Dec_path, Dis_path])
         else:
-            self.model_kept.append([Dis_path])
+            all_model = {
+                'encoder_s': self.Encoder_s.state_dict(),
+                'encoder_c': self.Encoder_c.state_dict(),
+                'decoder': self.Decoder.state_dict(),
+            }
+        new_model_path = '{}-{}'.format(model_path, iteration)
+        with open(new_model_path, 'wb') as f_out:
+            pickle.dump(all_model, f_out)
+        self.model_kept.append(new_model_path)
+
         if len(self.model_kept) >= self.max_keep:
-            for model_path in self.model_kept[0]:
-                os.remove(model_path)
+            os.remove(self.model_kept[0])
             self.model_kept.pop(0)
 
     def reset_grad(self):
@@ -97,58 +81,90 @@ class Solver(object):
         self.Decoder.zero_grad()
         self.Discriminator.zero_grad()
 
-    def train(self, model_path):
-        for iteration in range(self.hps.iterations):
-            #===================== Train D =====================#
-            X_i_t, X_i_tk, X_j = [self.to_var(x) for x in next(self.data_loader)]
-            # encode
-            Ec_i_t, Ec_i_tk, Ec_j = self.Encoder_c(X_i_t), self.Encoder_c(X_i_tk), self.Encoder_c(X_j)
-            # train discriminator
-            loss_adv_dis = -torch.mean(
-                torch.log(self.Discriminator(Ec_i_t, Ec_i_tk)) +
-                torch.log(1 - self.Discriminator(Ec_i_t, Ec_j))
-            )
-            self.reset_grad()
-            (self.hps.beta * L_adv_dis).backward()
-            self.D_opt.step()
+    def load_model(self, model_path, suffix, enc_only=False):
+        print('load model from {}'.format(model_path))
+        with open(model_path, 'rb') as f_in:
+            all_model = pickle.load(f_in)
+            self.Encoder_s.load_state_dict(all_model['encoder_s'])
+            self.Encoder_c.load_state_dict(all_model['encoder_c'])
+            self.Decoder.load_state_dict(all_model['decoder'])
+            if not enc_only:
+            self.Discriminator.load_state_dict(all_model['discriminator'])
+
+
+    def train(self, model_path, is_pretrain=False):
+        batch_size = self.hps.batch_size
+        pretrain_iterations, iterations = self.hps.pretrain_iterations, self.hps.iterations
+        alpha, beta1, beta2 = self.hps.alpha, self.hps.beta1, self.hps.beta2
+        for iteration in range(pretrain_iterations if is_pretrain else iterations):
+            if not is_pretrain:
+                #===================== Train D =====================#
+                X_i_t, X_i_tk, X_j = [self.to_var(x) for x in next(self.data_loader)]
+                # encode
+                Ec_i_t, Ec_i_tk, Ec_j = self.Encoder_c(X_i_t), self.Encoder_c(X_i_tk), self.Encoder_c(X_j)
+                # train discriminator
+                loss_adv_dis = torch.mean(
+                    (self.Discriminator(Ec_i_t, Ec_i_tk) - 1) ** 2 +
+                    self.Discriminator(Ec_i_t, Ec_j) ** 2
+                )
+                self.reset_grad()
+                (beta1 * loss_adv_dis).backward()
+                self.D_opt.step()
+                # print info
+                slot_value = (
+                    iteration,
+                    iterations,
+                    loss_adv_dis.data[0],
+                )
+                print(
+                    'D-iteration:[%06d/%06d], loss_adv_dis=%.3f' 
+                    % slot_value,
+                )
+                info = {
+                    'loss_adv_dis':loss_adv_dis.data[0],
+                }
+                for tag, value in info.items():
+                    self.logger.scalar_summary(tag, value, iteration + 1)
             #===================== Train G =====================#
-            # train encoder and decoder
             X_i_t, X_i_tk, X_j = [self.to_var(x) for x in next(self.data_loader)]
             # encode
             Es_i_t = self.Encoder_s(X_i_t)
             Es_i_tk = self.Encoder_s(X_i_tk)
             Ec_i_tk = self.Encoder_c(X_i_tk)
-            loss_sim = torch.mean((Es_i_t - Es_i_tk) ** 2)
+            loss_sim = torch.sum((Es_i_t - Es_i_tk) ** 2) / batch_size
             E = torch.cat([Es_i_t, Ec_i_tk], dim=1)
             X_tilde = self.Decoder(E)
-            loss_rec = torch.mean((X_tilde - X_i_tk) ** 2)
-            loss_adv_enc = -torch.mean(
-                0.5 * torch.log(self.Discriminator(Ec_i_tk, Ec_i_tk)) + 
-                0.5 * torch.log(1 - self.Discriminator(Ec_i_tk, Ec_i_tk))
-            )
-            loss = loss_rec + self.hps.alpha * loss_sim + self.hps.beta * loss_adv_enc
+            loss_rec = torch.sum((X_tilde - X_i_tk) ** 2) / batch_size
+            if not is_pretrain:
+                Ec_val = self.Discriminator(Ec_i_tk, Ec_i_tk)
+                mean_Ec_val = torch.mean(Ec_val)
+                loss_adv_enc = torch.mean(
+                    (Ec_val - 0.5) ** 2
+                )
+                loss = loss_rec + alpha * loss_sim + beta2 * loss_adv_enc
+            else:
+                loss = loss_rec + alpha * loss_sim
             self.reset_grad()
             loss.backward()
             self.G_opt.step()
             # print info
             slot_value = (
                 iteration,
-                self.hps.iterations,
+                pretrain_iterations if is_pretrain else iterations,
                 loss_rec.data[0],
                 loss_sim.data[0],
-                loss_adv_dis.data[0],
-                loss_adv_enc.data[0],
+                0 if is_pretrain else loss_adv_enc.data[0],
+                0 if is_pretrain else mean_Ec_val.data[0],
             )
             print(
-                'iteration:[%06d/%06d], loss_rec=%.3f, loss_sim=%.3f, '
-                'loss_adv_dis=%.3f, loss_adv_enc=%.3f' 
+                'G-iteration:[%06d/%06d], loss_rec=%.3f, loss_sim=%.3f, loss_adv_enc=%.3f, mean_val=%.3f' 
                 % slot_value,
             )
             info = {
                 'loss_rec':loss_rec.data[0],
                 'loss_sim':loss_sim.data[0],
-                'loss_adv_dis':loss_adv_dis.data[0],
-                'loss_adv_enc':loss_adv_enc.data[0],
+                'loss_adv_enc': 0 if is_pretrain else loss_adv_enc.data[0],
+                'mean_val': 0 if is_pretrain else mean_Ec_val.data[0],
             }
             for tag, value in info.items():
                 self.logger.scalar_summary(tag, value, iteration + 1)
