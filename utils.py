713c7d1846ea39915b43ac639fd866feac2b4a22
Jerry Chou <jjery2243542@gmail.com>
2017-11-23 01:58:47 +0800

    turn to index

--- utils.py
+++ utils.py
@@ -10,7 +10,9 @@ import argparse
 import random
 import time
 import torch
+from torch.utils import data
 from tensorboardX import SummaryWriter
+
 class Hps(object):
     def __init__(self):
         self.hps = namedtuple('hps', [
@@ -20,11 +22,14 @@ class Hps(object):
             'lambda_',
             'max_grad_norm',
             'max_step',
+            'seg_len',
+            'D_iterations',
             'batch_size',
+            'pretrain_iterations',
             'iterations',
             ]
         )
-        default = [5e-4, 1, 1, 1, 1, 5, 16, 15000]
+        default = [5e-4, 1, 0.5, 10, 1, 5, 128, 5, 16, 10000, 15000]
         self._hps = self.hps._make(default)
 
     def get_tuple(self):
@@ -54,9 +59,12 @@ class Sampler(object):
         self.read_sex_file(speaker_sex_path)
         self.utt2len = self.read_utt_len_file(utt_len_path)
         self.speakers = list(self.f_h5['train'].keys())
-        self.speaker2utts = {speaker:list(self.f_h5['train/{}'.format(speaker)].keys()) for speaker in self.speakers}
+        self.speaker_used = self.speakers[:8]
+        self.speaker2utts = {speaker:list(self.f_h5['train/{}'.format(speaker)].keys()) \
+                for speaker in self.speakers}
         # remove too short utterence
         self.rm_too_short_utt()
+        self.indexer = namedtuple('index', ['i', 'j', 't', 't_k', 't_k_prime', 't_j'])
 
     def read_utt_len_file(self, utt_len_path):
         with open(utt_len_path, 'r') as f:
@@ -97,7 +105,7 @@ class Sampler(object):
         seg_len = self.seg_len
         max_step = self.max_step
         # sample two speakers
-        speakerA, speakerB = random.sample(self.speakers, 2)
+        speakerA, speakerB = random.sample(self.speaker_used, 2)
         A_utt_id, A_len = self.sample_utt(speakerA)
         B_utt_id, B_len = self.sample_utt(speakerB)
         # sample t and t^k 
@@ -106,36 +114,63 @@ class Sampler(object):
         t_k_prime = random.randint(t + seg_len, min(A_len - seg_len, t + max_step * seg_len))
         # sample a segment from speakerB
         t_j = random.randint(0, B_len - seg_len)
-        return self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t:t + seg_len], \
-            self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t:t + seg_len],\
-            self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t_k:t_k + seg_len],\
-            self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t_k:t_k + seg_len],\
-            self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t_k_prime:t_k_prime + seg_len],\
-            self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t_k_prime:t_k_prime + seg_len],\
-            self.f_h5['train/{}/{}/mel'.format(speakerB, B_utt_id)][t_j:t_j + seg_len],\
-            self.f_h5['train/{}/{}/lin'.format(speakerB, B_utt_id)][t_j:t_j + seg_len]
+        index_tuple = self.indexer(i='{}/{}'.format(speakerA, A_utt_id), \
+                j='{}/{}'.format(speakerB, B_utt_id), \
+                t=t, t_k=t_k, t_k_prime=t_k_prime, t_j=t_j)
+        return index_tuple
+        #return self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t:t + seg_len], \
+        #    self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t:t + seg_len],\
+        #    self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t_k:t_k + seg_len],\
+        #    self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t_k:t_k + seg_len],\
+        #    self.f_h5['train/{}/{}/mel'.format(speakerA, A_utt_id)][t_k_prime:t_k_prime + seg_len],\
+        #    self.f_h5['train/{}/{}/lin'.format(speakerA, A_utt_id)][t_k_prime:t_k_prime + seg_len],\
+        #    self.f_h5['train/{}/{}/mel'.format(speakerB, B_utt_id)][t_j:t_j + seg_len],\
+        #    self.f_h5['train/{}/{}/lin'.format(speakerB, B_utt_id)][t_j:t_j + seg_len]
 
-class DataLoader(object):
-    def __init__(self, h5py_path, batch_size=16):
-        self.f_h5 = h5py.File(h5py_path)
-        self.keys = list(self.f_h5.keys())
-        self.index = 0
-        self.batch_size = batch_size
+#class DataLoader(object):
+#    def __init__(self, h5py_path, batch_size=16):
+#        self.f_h5 = h5py.File(h5py_path)
+#        self.keys = list(self.f_h5.keys())
+#        self.index = 0
+#        self.batch_size = batch_size
+#
+#    def __iter__(self):
+#        return self
+#
+#    def __next__(self):
+#        if self.index >= len(self.keys):
+#            self.index = 0
+#        key = self.keys[self.index]
+#        data = (self.f_h5['{}/X_i_t/mel'.format(key)][0:self.batch_size],
+#            self.f_h5['{}/X_i_tk/mel'.format(key)][0:self.batch_size],
+#            self.f_h5['{}/X_i_tk_prime/mel'.format(key)][0:self.batch_size],
+#            self.f_h5['{}/X_j/mel'.format(key)][0:self.batch_size])
+#        self.index += 1
+#        return data
 
-    def __iter__(self):
-        return self
+class myDataset(data.Dataset):
+    def __init__(self, h5_path, index_path, seg_len=128):
+        self.h5 = h5py.File(h5_path, 'r')
+        with open(index_path) as f_index:
+            self.indexes = json.load(f_index)
 
-    def __next__(self):
-        if self.index >= len(self.keys):
-            self.index = 0
-        key = self.keys[self.index]
-        data = (self.f_h5['{}/X_i_t/mel'.format(key)][0:self.batch_size],
-            self.f_h5['{}/X_i_tk/mel'.format(key)][0:self.batch_size],
-            self.f_h5['{}/X_i_tk_prime/mel'.format(key)][0:self.batch_size],
-            self.f_h5['{}/X_j/mel'.format(key)][0:self.batch_size])
-        self.index += 1
-        return data
-    
+        self.seg_len = seg_len
+
+    def __getitem__(self, i):
+        index = self.indexes[i]
+        i, j,= index['i'], index['j'] 
+        t, t_k, t_k_prime, t_j = index['t'], index['t_k'], index['t_k_prime'], index['t_j']
+        seg_len = self.seg_len
+        data = []
+        data.append(self.h5['train/{}/mel'.format(i)][t:t+seg_len])
+        data.append(self.h5['train/{}/mel'.format(i)][t_k:t_k+seg_len])
+        data.append(self.h5['train/{}/mel'.format(i)][t_k_prime:t_k_prime+seg_len])
+        data.append(self.h5['train/{}/mel'.format(j)][t_j:t_j+seg_len])
+        return tuple(data)
+
+    def __len__(self):
+        return len(self.indexes)
+
 class Logger(object):
     def __init__(self, log_dir='./log'):
         self.writer = SummaryWriter(log_dir)
@@ -144,30 +179,18 @@ class Logger(object):
         self.writer.add_scalar(tag, value, step)
 
 if __name__ == '__main__':
-    #sampler = Sampler(h5_path='/storage/raw_feature/voice_conversion/tacotron_feature/train-clean-100.h5', \
-    #    speaker_sex_path='/storage/raw_feature/voice_conversion/train-clean-100-speaker-sex.txt', \
-    #    max_step=5,\
-    #    seg_len=128
-    #)
-
-    #st = time.time()
-    #for _ in range(500):
-    #    datas = sampler.sample()
-    #et = time.time()
-    #print(et - st)
     hps = Hps()
     hps.dump('./hps/v4.json')
-    #data_loader = DataLoader('/storage/raw_feature/voice_conversion/two_speaker_16_5.h5')
-    #for _ in range(10):
-    #    print(next(data_loader))
-    #st = time.time()
-    #for i in range(100):
-    #    print(i)
-    #    batch = next(data_loader)
-    #et = time.time()
-    #print(et - st)
-    #logger = Logger()
-    #for i in range(100):
-    #    logger.scalar_summary('loss', np.random.randn(), i + 1)
+    dataset = myDataset('/storage/raw_feature/voice_conversion/tacotron_feature/train-clean-100.h5',\
+            '/storage/librispeech_index/200k.json')
+    def merge(data_list):
+        [data[0] for data in data_list]
+    data_loader = data.DataLoader(
+            dataset=dataset,
+            batch_size=16,
+            shuffle=False)
+    for i, batch in enumerate(data_loader):
+        print(batch)
+
 
 
