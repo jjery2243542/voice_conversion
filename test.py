739cd16641c2ee55dac5ee972f22f0891db3c8d5
Jerry Chou <jjery2243542@gmail.com>
2018-01-14 00:55:22 +0800

    change directory

--- test.py
+++ test.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     hps = Hps()
     hps.load('./hps/v16.json')
     hps_tuple = hps.get_tuple()
-    dataset = myDataset('/storage/raw_feature/voice_conversion/vctk/log_vctk.h5',\
-            '/storage/raw_feature/voice_conversion/vctk/128_513_2000k.json')
+    dataset = myDataset('/storage/feature/voice_conversion/vctk/log_vctk.h5',\
+            '/storage/feature/voice_conversion/vctk/128_513_2000k.json')
     data_loader = DataLoader(dataset)
     solver = Solver(hps_tuple, data_loader)
-    solver.load_model('/storage/model/voice_conversion/log_model.pkl-59999')
+    solver.load_model('/storage/model/voice_conversion/ae/log_model.pkl-59999')
     spec = np.loadtxt('./preprocess/test_code/lin.npy')
     spec2 = np.loadtxt('./preprocess/test_code/lin2.npy')
     spec_expand = np.expand_dims(spec, axis=0)
