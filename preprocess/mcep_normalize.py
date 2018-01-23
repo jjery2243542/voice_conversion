4f8edcf4e4e293035e1131f8c35e894b05c95977
Jerry Chou <jjery2243542@gmail.com>
2018-01-24 01:15:07 +0800

    normalize MCEP, test turn to spectrogram

--- preprocess/mcep_normalize.py
+++ preprocess/mcep_normalize.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import sys
 
 if __name__ == '__main__':
     in_h5py_path=sys.argv[1]
@@ -9,15 +10,14 @@ if __name__ == '__main__':
         for speaker in f_in['train'].keys():
             print(f'processing speaker_id={speaker}')
             # normalized f0
-            utts_f0 = [f_in[f'train/{speaker}/{utt_id}/log_f0'][:] for utt_id in f_in[f'train/{speaker}'].keys()]
+            utt_f0 = [f_in[f'train/{speaker}/{utt_id}/log_f0'][:] for utt_id in f_in[f'train/{speaker}'].keys()]
             all_f0 = np.concatenate(utt_f0, axis=0)
             print(all_f0.shape)
             f0_mean, f0_std = np.mean(all_f0), np.std(all_f0)
-            print(f0_mean.shape, f0_std.shape)
             f_out.create_dataset(f'{speaker}/f0_mean', data=f0_mean, dtype=np.float32)
             f_out.create_dataset(f'{speaker}/f0_std', data=f0_std, dtype=np.float32)
             # processing MCEP
-            utts_mc = [f_in[f'train/{speaker}/{utt_id}/mc'][:] for utt_id in f_in[f'train/{speaker}'].keys()]
+            utt_mc = [f_in[f'train/{speaker}/{utt_id}/mc'][:] for utt_id in f_in[f'train/{speaker}'].keys()]
             all_mc = np.concatenate(utt_mc, axis=0)
             print(all_mc.shape)
             mc_mean, mc_std = np.mean(all_mc, axis=0), np.std(all_mc, axis=0)
@@ -25,12 +25,12 @@ if __name__ == '__main__':
             f_out.create_dataset(f'{speaker}/mc_mean', data=mc_mean, dtype=np.float32)
             f_out.create_dataset(f'{speaker}/mc_std', data=mc_std, dtype=np.float32)
 
-       
-       for speaker in f_in['train'].keys():
-           print(f'normalize speaker_id={speaker}')
-           mc_mean = f_out[f'{speaker}/mc_mean'][:] 
-           mc_std = f_out[f'{speaker}/mc_std'][:]
-           for dset in ['train', 'test']:
-               mc = f_in[f'{dset}/{speaker}/{utt_id}/mc'][:]
-               norm_mc = (mc - mc_mean) / mc_std
-               f_in.create_dataset(f'{speaker}/norm_mc', data=norm_mc, dtype=np.float32)
+        for speaker in f_in['train'].keys():
+            print(f'normalize speaker_id={speaker}')
+            mc_mean = f_out[f'{speaker}/mc_mean'][:] 
+            mc_std = f_out[f'{speaker}/mc_std'][:]
+            for dset in ['train', 'test']:
+                for utt_id in f_in[f'{dset}/{speaker}']:
+                    mc = f_in[f'{dset}/{speaker}/{utt_id}/mc'][:]
+                    norm_mc = (mc - mc_mean) / mc_std
+                    f_in.create_dataset(f'{dset}/{speaker}/{utt_id}/norm_mc', data=norm_mc, dtype=np.float32)
--- test.py
+++ test.py
@@ -13,8 +13,6 @@ from preprocess.tacotron.utils import spectrogram2wav
 #from preprocess.tacotron.audio import inv_spectrogram, save_wav
 from scipy.io.wavfile import write
 
-def np2tensor(np_array):
-
 if __name__ == '__main__':
     hps = Hps()
     hps.load('./hps/v18.json')
@@ -22,9 +20,11 @@ if __name__ == '__main__':
     solver = Solver(hps_tuple, None)
     solver.load_model('/storage/model/voice_conversion/v18/model.pkl-44999')
     # indexer to extract data
-    indexer = Indexer('/storage/feature/voice_conversion/vctk/en_mcep_vctk.h5')
-    src_mc = indexer(speaker_id='225', utt_id='366', dset='test', feature='mc')
-    tar_mc = indexer(speaker_id='226', utt_id='366', dset='test', feature='mc')
+    #indexer = Indexer('/storage/feature/voice_conversion/vctk/en_mcep_vctk.h5')
+    #src_mc = indexer(speaker_id='225', utt_id='366', dset='test', feature='mc')
+    #tar_mc = indexer(speaker_id='226', utt_id='366', dset='test', feature='mc')
+    spec = np.loadtxt('preprocess/test_code/lin.npy')
+    spec2 = np.loadtxt('preprocess/test_code/lin2.npy')
     spec_expand = np.expand_dims(spec, axis=0)
     spec_tensor = torch.from_numpy(spec_expand)
     spec_tensor = spec_tensor.type(torch.FloatTensor)
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     spec2_tensor = torch.from_numpy(spec2_expand)
     spec2_tensor = spec2_tensor.type(torch.FloatTensor)
     c1 = Variable(torch.from_numpy(np.array([0]))).cuda()
-    c2 = Variable(torch.from_numpy(np.array([1]))).cuda()
+    c2 = Variable(torch.from_numpy(np.array([4]))).cuda()
     results = [spec, spec2]
     result = solver.test_step(spec_tensor, c1)
     result = result.squeeze(axis=0).transpose((1, 0))
@@ -50,5 +50,3 @@ if __name__ == '__main__':
         result = np.power(np.e, result)**1.2
         wav_data = spectrogram2wav(result)
         write(f'output{i+1}.wav', rate=16000, data=wav_data)
-
-
