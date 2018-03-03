2ac81338976263aa3edee6e91f4e314edfc4480e
Jerry Chou <jjery2243542@gmail.com>
2018-03-03 18:46:48 +0800

    for preprocess

---
+++ preprocess/h5_to_pkl.py
@@ -0,0 +1,20 @@
+import h5py
+import numpy as np
+import pickle 
+
+h5_path='/storage/feature/voice_conversion/vcc/trim_log_pad.h5'
+pkl_path = '/storage/feature/voice_conversion/vcc/trim_log_pad.pkl'
+speakers = ['SF1', 'SF2', 'SF3', 'SM1', 'SM2', 'TF1', 'TF2', 'TM1', 'TM2', 'TM3']
+
+dictionary = {}
+
+with h5py.File(h5_path, 'r') as f_h5:
+    for dset in ['train', 'test']:
+        for speaker in speakers:
+            print(f'processing speaker-{speaker}')
+            for utt_id in f_h5[f'{dset}/{speaker}']:
+                dictionary[f'{dset}/{speaker}/{utt_id}/mel'] = f_h5[f'{dset}/{speaker}/{utt_id}/mel'][()]
+                dictionary[f'{dset}/{speaker}/{utt_id}/lin'] = f_h5[f'{dset}/{speaker}/{utt_id}/lin'][()]
+
+with open(pkl_path, 'wb') as f:
+    pickle.dump(dictionary, f)
---
+++ preprocess/make_single_samples.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../')
+from utils import Sampler
+import h5py
+import numpy as np
+import json 
+
+max_step=5
+seg_len=128
+mel_band=80
+lin_band=513
+n_samples=20000
+dset='test'
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage: python3 make_single_samples.py [in_h5py_path] [out_json_path]')
+        exit(0)
+    sampler = Sampler(sys.argv[1], max_step=max_step, seg_len=seg_len, dset=dset)
+    samples = [sampler.sample_single()._asdict() for _ in range(n_samples)]
+    with open(sys.argv[2], 'w') as f_json:
+        json.dump(samples, f_json, indent=4, separators=(',', ': '))
